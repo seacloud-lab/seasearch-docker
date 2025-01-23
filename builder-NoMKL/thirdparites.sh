fac967b0b5d6d59d7a738fddcc93164e7780de49
Junxiang Huang <wacmkxiaoyi@gmail.com>
2025-01-23 10:56:45 +0800

    Update libraries version

--- builder-NoMKL/thirdparites.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir lib
-cd lib
-wget https://github.com/Kitware/CMake/releases/download/v3.29.3/cmake-3.29.3-linux-x86_64.sh
-wget https://nodejs.org/dist/v20.13.1/node-v20.13.1-linux-x64.tar.xz
-wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
