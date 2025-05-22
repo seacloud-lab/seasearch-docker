88b67435eceaf35a0055f9fcbcb73676f0f041ff
Junxiang Huang <wacmkxiaoyi@gmail.com>
2025-05-22 17:01:17 +0800

    update s3 confs

--- seasearch-NoMKL/entrypoint.sh
+++ seasearch-NoMKL/entrypoint.sh
@@ -7,6 +7,23 @@ function set_env(){
   export ZINC_ETCD_PASSWORD=$SS_ETCD_PASSWORD
   export ZINC_ETCD_ENDPOINTS=$SS_ETCD_ENDPOINTS
   export ZINC_ETCD_PREFIX=$SS_ETCD_PREFIX
+
+  if [ "$USE_S3_STORAGE" = "true" ]; then
+      local seafile_storage_type=s3
+  else
+      local seafile_storage_type=disk
+  fi
+
+  export SS_STORAGE_TYPE=${SS_STORAGE_TYPE:-$seafile_storage_type}
+  export SS_S3_BUCKET=${SS_S3_BUCKET:-${S3_SS_BUCKET:-}}
+  export SS_S3_ACCESS_ID=${SS_S3_ACCESS_ID:-${S3_KEY_ID:-}}
+  export SS_S3_USE_V4_SIGNATURE=${SS_S3_USE_V4_SIGNATURE:-${S3_USE_V4_SIGNATURE:-true}}
+  export SS_S3_ACCESS_SECRET=${SS_S3_ACCESS_SECRET:-${S3_SECRET_KEY:-}}
+  export SS_S3_ENDPOINT=${SS_S3_ENDPOINT:-${S3_HOST:-}}
+  export SS_S3_USE_HTTPS=${SS_S3_USE_HTTPS:-${S3_USE_HTTPS:-true}}
+  export SS_S3_PATH_STYLE_REQUEST=${SS_S3_PATH_STYLE_REQUEST:-${S3_PATH_STYLE_REQUEST:-true}}
+  export SS_S3_AWS_REGION=${SS_S3_AWS_REGION:-${S3_AWS_REGION:-us-east-1}}
+  export SS_S3_SSE_C_KEY=${SS_S3_SSE_C_KEY:-${S3_SSE_C_KEY:-}}
 }
 
 #start server
