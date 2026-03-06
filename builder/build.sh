61e1df7bc4403edbfdfb04de0dedd382e49a915e
Junxiang Huang <wacmkxiaoyi@gmail.com>
2026-03-06 14:52:43 +0800

    update seasearch docker: support seasearch cluster

--- builder/build.sh
+++ builder/build.sh
@@ -1,19 +1,24 @@
 #!/bin/bash  
    
 main() {  
-    local version=$1  
-      
-    cd /opt/seasearch/web || exit 1  
-     
+    local version=$1
+
+    cd /opt/seasearch/web || exit 1
+
     npm install  
-    npm run build  
-      
-    cd /opt/seasearch || exit 1  
-    export CGO_ENABLED=1  
-    go mod tidy  
-    go build -o seasearch ./cmd/zincsearch/  
+    npm run build
+
+    cd /opt/seasearch || exit 1
+    export CGO_ENABLED=1
+    go mod tidy
+    go build -o seasearch ./cmd/zincsearch/
+    go build -o seasearch-proxy ./cmd/zinc-proxy/main.go
+    go build -o cluster-manager ./cmd/cluster-manager/main.go
+
+    mv seasearch-proxy /opt/seasearch-proxy
+    mv cluster-manager /opt/cluster-manager
     
-    tar -czvf seasearch-$version.tar.gz /opt/faiss/build /opt/seasearch/seasearch /opt/seasearch/assets
+    tar -czvf seasearch-$version.tar.gz /opt/faiss/build /opt/seasearch/seasearch /opt/seasearch/assets /opt/seasearch-cluster/seasearch-proxy /opt/seasearch-cluster/cluster-manager
       
     echo "seasearch-$version.tar.gz 打包完成."  
 }  
---
+++ seasearch/entrypoint/seasearch-proxy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#start server
+cd /opt
+chmod +x /opt/seasearch-proxy
+./seasearch-proxy
+
+echo "This is a idle script (infinite loop) to keep container running."
+
+function cleanup(){
+  kill -s SIGTERM $!
+  exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+while [ 1 ]
+do
+  sleep 60 & wait $!
+done
