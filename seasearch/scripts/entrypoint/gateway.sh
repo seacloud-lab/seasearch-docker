9fca17da9a7e2327a5f1402e9e418bb8a5b308d7
Junxiang Huang <wacmkxiaoyi@gmail.com>
2026-03-12 16:04:25 +0800

    update gateway entrypoint

--- seasearch/scripts/entrypoint/gateway.sh
+++ seasearch/scripts/entrypoint/gateway.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 
 function set_env(){
-  export SS_CLUSTER_MANAGER_HOST=127.0.0.1
+  export SS_CLUSTER_MANAGER_HOST=0.0.0.0
   export SS_CLUSTER_MANAGER_PORT=4081
   export SS_CLUSTER_MANAGER_URL=http://127.0.0.1:4081/
   export SS_CLUSTER_PROXY_HOST=0.0.0.0
   export SS_CLUSTER_PROXY_PORT=4082
 }
 
-#start server
-set_env
+function set_proxy_env(){
+  export SS_CLUSTER_PROXY_HOST=0.0.0.0
+  export SS_CLUSTER_PROXY_PORT=4082
+}
+
+function set_manager_env(){
+  export SS_CLUSTER_MANAGER_HOST=0.0.0.0
+  export SS_CLUSTER_MANAGER_PORT=4081
+}
+
 cd /opt
-chmod +x /opt/cluster/cluster-manager
-./cluster-manager &
-chmod +x /opt/cluster/seasearch-proxy
-./seasearch-proxy &
+
+case "${GATEWAY_NODE_TYPE}" in
+  "proxy")
+    echo "Starting in PROXY mode"
+    set_proxy_env
+    chmod +x /opt/cluster/seasearch-proxy
+    ./seasearch-proxy &
+    ;;
+    
+  "manager")
+    echo "Starting in MANAGER mode"
+    set_manager_env
+    chmod +x /opt/cluster/cluster-manager
+    ./cluster-manager &
+    ;;
+    
+  *)
+    echo "Starting in NORMAL mode"
+    set_env
+    chmod +x /opt/cluster/cluster-manager
+    ./cluster-manager &
+    chmod +x /opt/cluster/seasearch-proxy
+    ./seasearch-proxy &
+    ;;
+esac
 
 echo "This is a idle script (infinite loop) to keep container running."
 
