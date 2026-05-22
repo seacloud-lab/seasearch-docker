adeee423ba8d9f2d19b0ed3eb5f6976b17bf2b56
Junxiang Huang <wacmkxiaoyi@gmail.com>
2026-05-22 14:17:12 +0800

    update basic lib version & builder base image version

--- builder/thirdparites.sh
+++ builder/thirdparites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir lib
 cd lib
-wget https://github.com/Kitware/CMake/releases/download/v4.0.2/cmake-4.0.2-linux-x86_64.sh
-wget https://nodejs.org/dist/v22.16.0/node-v22.16.0-linux-x64.tar.xz
-wget https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v4.3.3/cmake-4.3.3-linux-x86_64.sh
+wget https://nodejs.org/dist/v26.2.0/node-v26.2.0-linux-x64.tar.xz
+wget https://go.dev/dl/go1.26.3.linux-amd64.tar.gz
